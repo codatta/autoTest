9c92e8f8634a7f2091f5ef8c49525b4ffebcccf9
lh <2928446205@qq.com>
2024-12-05 17:18:15 +0800

    小改

--- run_tests.py
+++ run_tests.py
@@ -10,7 +10,8 @@ def run_tests():
 if __name__ == "__main__":
     while True:
         exit_code = run_tests()  # 运行测试并获取退出代码
+        time.sleep(100)
         if exit_code != 0:
-            print("测试失败")  # 可以根据需要添加更多处理逻辑
+            print("有测试并未通过")  # 可以根据需要添加更多处理逻辑
         else:
             print("所有测试通过")
