510707716e14a0bbfd2cd03852386316eb829a62
lh <2928446205@qq.com>
2024-12-07 13:34:24 +0800

    基础工具类优化

--- run_tests.py
+++ run_tests.py
@@ -7,16 +7,6 @@ from utils.logger import setup_logging
 
 
 def run_tests():
-    # 创建定时任务调度器
-    scheduler = BackgroundScheduler()
-    # 添加定时任务，这里设置为每天凌晨2点执行删除过期截图的任务，你可以根据需求调整触发时间
-    scheduler.add_job(delete_old_screenshots, 'cron', hour=2, minute=0, args=[Config.SCREENPATH])
-    # 添加定时任务，设置为每天凌晨3点执行删除过期日志的任务，可根据实际需求调整触发时间
-    scheduler.add_job(delete_old_logs, 'cron', hour=3, minute=0, args=[Config.LOG_PATH])
-    # 添加定时任务，设置为每天凌晨4点执行删除过期报告的任务，可根据需求调整触发时间
-    scheduler.add_job(delete_old_reports, 'cron', hour=4, minute=0, args=[Config.REPORT_PATH])  # 添加删除报告定时任务
-    # 启动定时任务调度器
-    scheduler.start()
     # 运行所有测试用例
     # 获取当前时间戳，用于区分不同轮次的测试报告文件名
     timestamp = time.strftime("%Y-%m-%d_%H-%M", time.localtime())
@@ -29,6 +19,16 @@ def run_tests():
 if __name__ == "__main__":
     logger = setup_logging()
     logger.info(".........这是一轮整体用例测试的开始.........")
+    # 创建定时任务调度器
+    scheduler = BackgroundScheduler()
+    # 添加定时任务，这里设置为每天凌晨2点执行删除过期截图的任务，你可以根据需求调整触发时间
+    scheduler.add_job(delete_old_screenshots, 'cron', hour=2, minute=0, args=[Config.SCREENPATH])
+    # 添加定时任务，设置为每天凌晨3点执行删除过期日志的任务，可根据实际需求调整触发时间
+    scheduler.add_job(delete_old_logs, 'cron', hour=3, minute=0, args=[Config.LOG_PATH])
+    # 添加定时任务，设置为每天凌晨4点执行删除过期报告的任务，可根据需求调整触发时间
+    scheduler.add_job(delete_old_reports, 'cron', hour=4, minute=0, args=[Config.REPORT_PATH])  # 添加删除报告定时任务
+    # 启动定时任务调度器
+    scheduler.start()
     while True:
         exit_code = run_tests()  # 运行测试并获取退出代码
         time.sleep(1)
